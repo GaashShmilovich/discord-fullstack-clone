48036a09d5f3481c610c3ee4cdeb213a719d5194
Gaash Shmilovich <gaash.shm@gmail.com>
2023-09-20 10:40:47 +0300

    finish leave server UI + logic

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -92,6 +92,7 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
                 {isModerator && <DropdownMenuSeparator />}
                 {isAdmin && (
                     <DropdownMenuItem
+                        onClick={() => onOpen('deleteServer', { server })}
                         className="text-rose-500 px-3 py-2 
                     text-sm cursor-pointer"
                     >
